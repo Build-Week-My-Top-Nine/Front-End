aa887c12ac9fafbadfbcf9e712e1b9fca916051a
Barbara Mayfield <bmayfield1991@gmail.com>
2019-09-18 21:02:27 -0500

    created SearchForm.js component, changed class names on App.js & reflected that on the App.css as well as removed some of the boilerplate css in that file, SearchForm rendering to app

--- top-nine/src/App.js
+++ top-nine/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import SearchForm from './components/SearchForm';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <body className="App-body">
+        <header className="App-header">
         <h1>TopNine</h1>
-      </header>
+        <SearchForm />
+        </header>
+      </body>
     </div>
   );
 }
