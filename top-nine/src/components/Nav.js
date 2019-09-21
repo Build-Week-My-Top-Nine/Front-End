80827c9082f54c9276c0fe3c7e67c2978df47c03
Barbara Mayfield <bmayfield1991@gmail.com>
2019-09-21 09:57:49 -0500

    separated nav section in app.js to its own component file

--- top-nine/src/components/Nav.js
+++ top-nine/src/components/Nav.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { Route } from 'react-router-dom'
 
+export default function Nav() {
+    return (
+        <NavBg>
+        <header className="App-header">
+          <h1>TopNine</h1>
+          <SearchForm />
+            <NavLink className="NavLink" to="/">
+              Home
+            </NavLink>
+            <NavLink className="NavLink" to="/About">
+              About
+            </NavLink>
+            <NavLink className="NavLink" to="/Login">
+              Login
+            </NavLink>
+            </header>
+        </NavBg>
+    )
+}
