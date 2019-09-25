2eedbf0fd25600971d2c18efe2a1d7cedb0e5b56
Barbara Mayfield <bmayfield1991@gmail.com>
2019-09-24 20:22:15 -0500

    created SearchForm to filter TopNineItems

--- top-nine/src/components/search/SearchForm.js
+++ top-nine/src/components/search/SearchForm.js
@@ -1,16 +1,44 @@
-import React  from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Field, withFormik } from 'formik';
 import { Button } from 'reactstrap';
+import axios from 'axios';
 
 
 
 function SearchForm() {
-    return (
-        <>
+
+  const [search, setSearchValue] = useState({
+    value: ''
+  })
+
+  const [items, setItems] = useState([])
+
+  const [filteredItems, setFilteredItems] = useState([])
+
+  useEffect((props) => {
+    axios.get("https://mytopnineapi.herokuapp.com/api/topnine").then(res => {
+      console.log(res.data, "PROP IN SEARCH");
+      // setSearchValue(res.data);
+      setItems(res.data);
+      setFilteredItems(res.data);
+    });
+  }, []);
+
+  function handleChange(e) {
+    const newItems = items.filter((item) => { 
+      console.log(item, "Item")
+     return item.TopNineItem.toLowerCase().includes(e.target.value)
+    })
+    setFilteredItems(newItems);
+  }
+  
+  return (
+    <>
         <Form className="searchform">
-            <Field type="text" name="search" placeholder="Search..." className="search-field" />
+            <Field type="text" name="search" placeholder="Search..." className="search-field" onChange={handleChange} />
             <Button className="search-button">Search</Button>
         </Form>
+        {filteredItems.map((item) => (<p class="searchitems">{item.TopNineItem}</p>))}
         </>
     );
 }
