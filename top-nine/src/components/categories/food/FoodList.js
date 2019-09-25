334712a40387f2af2ab120e1b6ff3e555ef8dfb2
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-09-24 19:21:41 -0600

    trying to handle checkboxes

--- top-nine/src/components/categories/food/FoodList.js
+++ top-nine/src/components/categories/food/FoodList.js
@@ -1,46 +1,59 @@
-import React from 'react';
-import {Form} from 'formik';
+import React, {useState} from "react";
+import { Form } from "formik";
 
 export default function FoodList(props) {
-    return (
-        <Form>
-        <h3>Food List</h3>
-        <label>
-            <input type="radio" value="Spaghetti"></input>
-            Spaghetti
-        </label>
-        <label>
-            <input type="radio" value="Croissant"></input>
-            Croissant
-        </label>
-        <label>
-            <input type="radio" value="Stew"></input>
-            Stew
-        </label>
-        <label>
-            <input type="radio" value="Seafood"></input>
-            Seafood
-        </label>
-        <label>
-            <input type="radio" value="Stuffed Peppers"></input>
-            Stuffed Peppers
-        </label>
-        <label>
-            <input type="radio" value="Tostones"></input>
-            Tostones
-        </label>
-        <label>
-            <input type="radio" value="Tacos"></input>
-            Tacos
-        </label>
-        <label>
-            <input type="radio" value="Chili"></input>
-            Chili
-        </label>
-        <label>
-            <input type="radio" value="Orange Chicken"></input>
-            Orange Chili
-        </label>
+  const [rememberMe, setRememberMe] = useState(false);
+  const handleChange = e => {
+      setRememberMe(!rememberMe);
+//     if (rememberMe === true) {
+//       setRememberMe(false);
+//     } else {
+//         setRememberMe(true);   
+// }};
+  }
+console.log(rememberMe);
+  return (
+    <Form onChange={handleChange}>
+      <h3>Food List</h3>
+      <label>
+        <input
+          type="checkbox"
+          value="Spaghetti"
+        ></input>
+        Spaghetti
+      </label>
+      <label>
+        <input type="checkbox" value="Croissant" onChange={(e) => setRememberMe(e.target.value)}></input>
+        Croissant
+      </label>
+      <label>
+        <input type="checkbox" value="Stew" onChange={(e) => setRememberMe(e.target.value)}></input>
+        Stew
+      </label>
+      <label>
+        <input type="checkbox" value="Seafood" onChange={(e) => setRememberMe(e.target.value)}></input>
+        Seafood
+      </label>
+      <label>
+        <input type="checkbox" value="Stuffed Peppers" onChange={(e) => setRememberMe(e.target.value)}></input>
+        Stuffed Peppers
+      </label>
+      <label>
+        <input type="checkbox" value="Tostones" onChange={(e) => setRememberMe(e.target.value)}></input>
+        Tostones
+      </label>
+      <label>
+        <input type="checkbox" value="Tacos" onChange={(e) => setRememberMe(e.target.value)}></input>
+        Tacos
+      </label>
+      <label>
+        <input type="checkbox" value="Chili" onChange={(e) => setRememberMe(e.target.value)}></input>
+        Chili
+      </label>
+      <label>
+        <input type="checkbox" value="Orange Chicken" onChange={(e) => setRememberMe(e.target.value)}></input>
+        Orange Chili
+      </label>
     </Form>
-    )
+  );
 }
